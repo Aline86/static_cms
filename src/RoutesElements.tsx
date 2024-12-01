19473eaf8fd4bee343648d49dc7e3662e3a2a994
Claire-Aline Haestie <ca.haestie@gmail.com>
2024-12-01 22:29:54 +0100

    some cleaning in RoutesElements

--- src/RoutesElements.tsx
+++ src/RoutesElements.tsx
@@ -11,28 +11,6 @@ import Visualization from "./admin/backoffice/page/page_template/page";
 import CommonVisualization from "./admin/backoffice/bloc/components/common/general_settings";
 
 export default function ThemeContextProvider({}: { children: any }) {
-  const [pages, setPages] = useState<Array<Page>>([]);
-  const getPages = async () => {
-    let new_pages: Array<Page> = [];
-    await axios
-      .get("http://localhost:80/cms_v2/api/index.php?method=get_pages")
-      .then((response) => {
-        let elements = response.data;
-        if (Array.isArray(elements) && elements.length >= 1) {
-          elements.forEach((element: { id: number; title: string }) => {
-            let new_page = new Page(element.id, element.title);
-            new_page !== null && new_pages.push(new_page);
-          });
-        }
-      })
-
-      .catch((err: any) => {
-        alert(err);
-      });
-
-    setPages(new_pages);
-  };
-  //   //path="/:name/:id" element=<Visualization />
   return (
     <HashRouter>
       <Routes>
